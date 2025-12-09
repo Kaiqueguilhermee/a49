542a0c8aa09137e05febb72feaf2f3feb0c32b9d
Kaiqueguilhermee <201179565+Kaiqueguilhermee@users.noreply.github.com>
2025-12-09 12:18:42 -0300

    TEST

--- app/Http/Controllers/Api/DrakonController.php
+++ app/Http/Controllers/Api/DrakonController.php
@@ -385,7 +385,7 @@ class DrakonController extends Controller
             ], 200);
         }
 
-        $wallet = Wallet::where('user_id', $userId)->where('active', 1)->first();
+        $wallet = Wallet::where('user_id', $userId)->first();
         
         if (!$wallet) {
             return response()->json(['status' => false, 'error' => 'INVALID_USER'], 200);
@@ -459,7 +459,7 @@ class DrakonController extends Controller
             ], 200);
         }
 
-        $wallet = Wallet::where('user_id', $userId)->where('active', 1)->first();
+        $wallet = Wallet::where('user_id', $userId)->first();
         
         if (!$wallet) {
             return response()->json(['status' => 0, 'balance' => 0], 200);
