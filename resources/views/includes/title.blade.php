41f51b6a39b3cb733a5964b8883dd29615b29ac4
Kaiqueguilhermee <201179565+Kaiqueguilhermee@users.noreply.github.com>
2025-12-11 10:35:41 -0300

    test

--- resources/views/includes/title.blade.php
+++ resources/views/includes/title.blade.php
@@ -1,10 +1,6 @@
 <div class="header-box">
     <div class="header-title">
-        @if($title == '+Jogados Da Semana')
-            <img src="https://cdn.7games.bet.br/content/assets/icons/real-money.png?q=0&lossless=1&h=20&w=20" alt="Dinheiro Real" style="width: 20px; height: 20px; margin-right: 10px;">
-        @else
-            <i class="{{ $icon }}" style="font-size: 23px;margin-right: 10px;color: #65cb24;"></i>
-        @endif
+        <i class="{{ $icon }}" style="font-size: 23px;margin-right: 10px;color: #65cb24;"></i>
         <h4>{{ $title }}</h4>
     </div>
     <div>
