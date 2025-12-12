49c6078fd19f20856539e079ceddc9735596ec52
Kaiqueguilhermee <201179565+Kaiqueguilhermee@users.noreply.github.com>
2025-12-12 16:38:06 -0300

    test

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -51,6 +51,12 @@ class CategoryResource extends Resource
                             ->directory('uploads')
                             ->visibility('public')
                             ->columnSpanFull(),
+                        Forms\Components\MultiSelect::make('games')
+                            ->label('Jogos desta categoria')
+                            ->options(\App\Models\Game::where('type', 'slots')->pluck('name', 'id'))
+                            ->relationship('games', 'name')
+                            ->helperText('Selecione os jogos que deseja associar a esta categoria. Somente jogos do tipo "slots" aparecem aqui.')
+                            ->columnSpanFull(),
                     ])
                 ]);
     }
