a5d6f6bfe4313156013a5db73fd8fd7277be3738
Kaiqueguilhermee <201179565+Kaiqueguilhermee@users.noreply.github.com>
2025-12-12 13:41:45 -0300

    tst

--- resources/views/components/winners-mobile.blade.php
+++ resources/views/components/winners-mobile.blade.php
@@ -1,41 +1,84 @@
 @php
-    // Pega jogos aleatórios do +Jogados da Semana
     $topGames = $topTrendGames ?? [];
-    $mockNames = ['Carlos S***', 'Maria L***', 'João P***', 'Ana B***', 'Pedro M***', 'Lucia F***', 'Roberto C***', 'Juliana R***', 'Marcos T***', 'Fernanda A***', 'Rafael D***', 'Patricia G***', 'Bruno H***', 'Camila N***', 'Diego V***'];
+    $mockNames = ['Jefferson F***', 'Wu Z***', 'Celso L***', 'Marco S***', 'Jose M***', 'Cintia A***', 'Elayne C***', 'Matheus C***', 'Patricia G***', 'Bruno H***', 'Camila N***', 'Diego V***'];
+    $avatars = range(1, 40);
     
     $mockWinners = [];
-    for ($i = 0; $i < 15; $i++) {
+    for ($i = 0; $i < 20; $i++) {
         if (count($topGames) > 0) {
             $game = $topGames[$i % count($topGames)];
             $mockWinners[] = [
                 'name' => $mockNames[array_rand($mockNames)],
                 'game' => $game->name,
-                'amount' => 'R$ ' . number_format(rand(100, 999), 0, ',', '.') . '.000',
-                'image' => str_starts_with($game->image, 'http') ? $game->image : asset('storage/'.$game->image)
+                'amount' => 'R$ ' . number_format(rand(43, 154) * 1000 + rand(0, 999), 2, ',', '.'),
+                'image' => str_starts_with($game->image, 'http') ? $game->image : asset('storage/'.$game->image),
+                'avatar' => $avatars[array_rand($avatars)]
             ];
         }
     }
 @endphp
 
-<div class="winners-mobile-container">
-    <div class="winners-mobile-track">
-        @foreach($mockWinners as $winner)
-            <div class="winner-mobile-item">
-                <div class="winner-info-left">
-                    <p class="winner-name">{{ $winner['name'] }}</p>
-                    <p class="winner-amount">{{ $winner['amount'] }}</p>
+<div class="top-winners-container-mobile">
+    <!-- Tabs de Período -->
+    <div class="top-winners-tabs-container">
+        <div class="top-winners-tabs-text">
+            <img src="https://cdn.7games.bet.br/react-app/cms/images/icons/clock-two_home.svg" alt="Clock">
+            <p>Período:</p>
+        </div>
+        <div class="top-winners-tabs">
+            <div class="top-winners-tab active">Hoje</div>
+            <div class="top-winners-tab">3 Dias</div>
+            <div class="top-winners-tab">7 Dias</div>
+            <div class="top-winners-tab">15 Dias</div>
+            <div class="top-winners-tab">30 Dias</div>
+        </div>
+    </div>
+
+    <!-- Header com Troféu -->
+    <div class="top-winners-header">
+        <img src="https://cdn.7games.bet.br/content/assets/trofeu2.png?q=0&lossless=1&h=32&w=32" alt="Troféu">
+        <p>Top Ganhos</p>
+    </div>
+
+    <!-- Lista de Ganhadores com Scroll -->
+    <div class="top-winners-scroll">
+        <div class="top-winners-track">
+            @foreach($mockWinners as $winner)
+                <div class="top-winner-card">
+                    <div class="top-winner-game-img">
+                        <img src="{{ $winner['image'] }}" alt="{{ $winner['game'] }}">
+                    </div>
+                    <div class="top-winner-info">
+                        <div class="top-winner-player">
+                            <div class="top-winner-avatar">
+                                <img class="avatar-roll" src="https://cdn.7games.bet.br/content/assets/rodela.svg">
+                                <img class="avatar-img" src="https://cdn.7games.bet/content/images/avatars/v2/{{ $winner['avatar'] }}.webp">
+                            </div>
+                            <p class="player-name">{{ $winner['name'] }}</p>
+                        </div>
+                        <span class="game-name">{{ $winner['game'] }}</span>
+                        <p class="winner-amount">{{ $winner['amount'] }}</p>
+                    </div>
                 </div>
-                <img src="{{ $winner['image'] }}" alt="{{ $winner['game'] }}" class="winner-game-img">
-            </div>
-        @endforeach
-        @foreach($mockWinners as $winner)
-            <div class="winner-mobile-item">
-                <div class="winner-info-left">
-                    <p class="winner-name">{{ $winner['name'] }}</p>
-                    <p class="winner-amount">{{ $winner['amount'] }}</p>
+            @endforeach
+            @foreach($mockWinners as $winner)
+                <div class="top-winner-card">
+                    <div class="top-winner-game-img">
+                        <img src="{{ $winner['image'] }}" alt="{{ $winner['game'] }}">
+                    </div>
+                    <div class="top-winner-info">
+                        <div class="top-winner-player">
+                            <div class="top-winner-avatar">
+                                <img class="avatar-roll" src="https://cdn.7games.bet.br/content/assets/rodela.svg">
+                                <img class="avatar-img" src="https://cdn.7games.bet/content/images/avatars/v2/{{ $winner['avatar'] }}.webp">
+                            </div>
+                            <p class="player-name">{{ $winner['name'] }}</p>
+                        </div>
+                        <span class="game-name">{{ $winner['game'] }}</span>
+                        <p class="winner-amount">{{ $winner['amount'] }}</p>
+                    </div>
                 </div>
-                <img src="{{ $winner['image'] }}" alt="{{ $winner['game'] }}" class="winner-game-img">
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 </div>
