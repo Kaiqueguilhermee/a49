b540270a05c5927e5e8d76a3d04b1c8f5fa1777e
Kaiqueguilhermee <201179565+Kaiqueguilhermee@users.noreply.github.com>
2025-12-18 20:13:29 -0300

    test

--- public/assets/js/touch-listener-inspect.js
+++ public/assets/js/touch-listener-inspect.js
@@ -1,15 +1,2 @@
 // touch-listener-inspect.js
-// Versao ultra-simples para evitar qualquer erro de sintaxe ou codificacao.
-(function(){
-    if (window.location.search.indexOf('dbg_touch=1') === -1) return;
-    var origAdd = EventTarget.prototype.addEventListener;
-    var tipos = ['touchstart','touchmove','touchend','touchcancel','pointerdown','pointermove','pointerup','pointercancel','mousedown','mousemove','mouseup','click','dragstart','drag','dragend'];
-    EventTarget.prototype.addEventListener = function(tipo, listener, opcoes){
-        if(tipos.indexOf(tipo) !== -1){
-            if(window.console && window.console.info){
-                window.console.info('[touch-listener-inspect] tipo=' + tipo);
-            }
-        }
-        return origAdd.apply(this, arguments);
-    };
-})();
+// Arquivo limpo para teste de erro de sintaxe.
