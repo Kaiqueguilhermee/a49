6dedf48aa83a84059df77f32694e3519ecb521fa
Kaiqueguilhermee <201179565+Kaiqueguilhermee@users.noreply.github.com>
2025-12-18 20:28:30 -0300

    test

--- resources/views/web/home/index.blade.php
+++ resources/views/web/home/index.blade.php
@@ -108,7 +108,7 @@
                 @endif
                 {{-- Categorias marcadas para home --}}
                 @if(isset($categoriesHome) && count($categoriesHome) > 0)
-                    @foreach($categoriesHome as $category)
+                    @foreach($categoriesHome->take(6) as $category)
                         @if($category->gamesSlotgrator->count() > 0)
                             <div >
                                 @include('includes.title', ['link' => url('/category/'.$category->slug), 'title' => $category->name])
