bcc5e18650e9b3a74b6239c7121bff2ff3cd0f39
Kaiqueguilhermee <201179565+Kaiqueguilhermee@users.noreply.github.com>
2025-12-18 20:10:00 -0300

    cas

--- public/assets/js/touch-listener-inspect.js
+++ public/assets/js/touch-listener-inspect.js
@@ -1,18 +1,13 @@
 // touch-listener-inspect.js
-// Minimal version to avoid encoding issues and syntax errors.
+// Versão ultra-simples para evitar qualquer erro de sintaxe ou codificação.
 (function(){
     if (window.location.search.indexOf('dbg_touch=1') === -1) return;
     var origAdd = EventTarget.prototype.addEventListener;
-    var types = [
-        'touchstart','touchmove','touchend','touchcancel',
-        'pointerdown','pointermove','pointerup','pointercancel',
-        'mousedown','mousemove','mouseup','click','dragstart','drag','dragend'
-    ];
-    EventTarget.prototype.addEventListener = function(type, listener, options){
-        if(types.indexOf(type) !== -1){
-            var info = '[touch-listener-inspect] addEventListener: type=' + type;
+    var tipos = ['touchstart','touchmove','touchend','touchcancel','pointerdown','pointermove','pointerup','pointercancel','mousedown','mousemove','mouseup','click','dragstart','drag','dragend'];
+    EventTarget.prototype.addEventListener = function(tipo, listener, opcoes){
+        if(tipos.indexOf(tipo) !== -1){
             if(window.console && window.console.info){
-                window.console.info(info);
+                window.console.info('[touch-listener-inspect] tipo=' + tipo);
             }
         }
         return origAdd.apply(this, arguments);
